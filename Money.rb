b3dd7f6efbfbe591cbc433c30d89a032a48bd324
kris <kristophermiedema@gmail.com>
2023-08-13 20:01:09 -0400

    Adding comparators to money

--- Money.rb
+++ Money.rb
@@ -16,7 +16,42 @@ class DollarFixedPt
     return DollarFixedPt.new(@dollars, @cents)
   end
 
+  def >(obj)
+    return greater(obj)
+  end
+  def >=(obj)
+    return (greater(obj) || equal(obj))
+  end
+  def <=(obj)
+    return !greater(obj)
+  end
+  def <(obj)
+    return !(greater(obj) || equal(obj))
+  end
+  def ==(obj)
+    return equal(obj)
+  end
+  def !=(obj)
+    return !equal(obj)
+  end
+
   def to_s
     return "$#{@dollars}.#{@cents.to_s.rjust(2, "0")}"
+  end
+  
+  private
+  def equal(obj)
+    return @dollars == obj.dollars && @cents == obj.cents
+  end
+  def greater(obj)
+    result = false
+    if @dollars > obj.dollars
+      result = true
+    elsif @dollars == obj.dollars
+      if @cents > obj.cents
+        result = true
+      end
+    end
+    return result
   end
 end
